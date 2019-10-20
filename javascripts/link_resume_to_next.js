57956796cdba7a1228a8ea83fc8bc4605cbdb23f
mrbossosity <56628302+mrbossosity@users.noreply.github.com>
2019-10-19 18:35:18 -1000

    Update link_resume_to_next.js

--- javascripts/link_resume_to_next.js
+++ javascripts/link_resume_to_next.js
@@ -1,1 +1,11 @@
-
+const primary = document.getElementById('next');
+const secondary = document.getElementById('resume');
+function somePrimaryAction(e){
+  e.preventDefault();
+}
+function clickPrimaryButton(e){
+  e.preventDefault();
+  primary.click();
+}
+primary.addEventListener("click", somePrimaryAction, false);
+secondary.addEventListener("click", clickPrimaryButton, false);
