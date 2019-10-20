110866f89061fd47f5760488786f0850e864b013
mrbossosity <56628302+mrbossosity@users.noreply.github.com>
2019-10-19 18:36:53 -1000

    Update button_functions.js

--- javascripts/button_functions.js
+++ javascripts/button_functions.js
@@ -1,1 +1,30 @@
-
+function disablenext () {
+  document.getElementById("next").disabled=true;
+  }
+function enablenext () {
+  document.getElementById("next").disabled=false;
+  }
+function pause() {
+  speed = 1000000000;
+  }
+function resume() {
+  speed = 55;
+  }
+function buzz() {
+  speed = 10000000000;
+  }
+function skip() {
+  speed = 0
+  }
+function focusanswer() {
+  document.getElementById("answerbox").focus();
+  }
+function answerform(a) {
+  if(a==1)
+        document.getElementById("buzzanswer").style.display="none";
+  else
+        document.getElementById("buzzanswer").style.display="block";
+}
+ function reload() {
+  setTimeout(function(){location.reload(); }, 4000);
+}
