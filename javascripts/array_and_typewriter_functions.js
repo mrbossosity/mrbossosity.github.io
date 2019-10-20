a756144b3e5dc4280d43f1764a91b3d006027e96
mrbossosity <56628302+mrbossosity@users.noreply.github.com>
2019-10-19 21:45:17 -1000

    Update array_and_typewriter_functions.js

--- javascripts/array_and_typewriter_functions.js
+++ javascripts/array_and_typewriter_functions.js
@@ -1,4 +1,4 @@
-var myArray = src = "mrbossosity.github.io/JSON/
+var myArray = src = "mrbossosity.github.io/JSON/questions.json"
     var rand = myArray[Math.floor(Math.random() * myArray.length)];
     var i = 0;
     var speed = 60;
