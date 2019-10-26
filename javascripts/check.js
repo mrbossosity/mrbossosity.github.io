359c1b2e64882014646e8afe7bb3de8803baa0d8
mrbossosity <56628302+mrbossosity@users.noreply.github.com>
2019-10-26 01:06:07 -1000

    Create check.js

---
+++ javascripts/check.js
@@ -0,0 +1,10 @@
+function check() {
+var therealanswer = document.getElementById("typeanswer").innerHTML;
+var theanswer = document.getElementById("answerbox").value;
+if (therealanswer == theanswer) {
+  alert("Correct!")
+  }
+ else {
+  alert("Wrong!");
+  }
+};
