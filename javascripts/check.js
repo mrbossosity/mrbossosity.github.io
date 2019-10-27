b82386d7a8e83ab3db7e2ec68db0eb93d5e0ffa3
mrbossosity <56628302+mrbossosity@users.noreply.github.com>
2019-10-26 14:38:12 -1000

    Case insensitive

--- javascripts/check.js
+++ javascripts/check.js
@@ -1,7 +1,9 @@
 function check() {
 var therealanswer = document.getElementById("typeanswer").innerHTML;
+var TRAcaseinsensitive = therealanswer.toUpperCase();
 var theanswer = document.getElementById("answerbox").value;
-if (therealanswer == theanswer) {
+var TAcaseinsensitive = theanswer.toUpperCase();
+if (TRAcaseinsensitive == TAcaseinsensitive) {
   alert("Correct!")
   }
  else {
