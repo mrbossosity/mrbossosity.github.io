6607b5890887a1b54e8682bd38d038442d8522f2
mrbossosity <56628302+mrbossosity@users.noreply.github.com>
2019-10-26 20:28:45 -1000

    Update check.js

--- javascripts/check.js
+++ javascripts/check.js
@@ -3,7 +3,7 @@ var therealanswer = document.getElementById("typeanswer").innerHTML;
 var TRAcaseinsensitive = therealanswer.toUpperCase();
 var theanswer = document.getElementById("answerbox").value;
 var TAcaseinsensitive = theanswer.toUpperCase();
-if (TRAcaseinsensitive == TAcaseinsensitive) {
+if (TAcaseinsensitive.includes(TRAcaseinsensitive)) {
   alert("Correct!")
   }
  else {
