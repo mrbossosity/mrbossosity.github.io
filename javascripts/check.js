4923a93e149e0c9420dcea4161ce279ea617b838
mrbossosity <56628302+mrbossosity@users.noreply.github.com>
2019-10-27 22:14:12 -1000

    Update check.js

--- javascripts/check.js
+++ javascripts/check.js
@@ -3,12 +3,8 @@ var therealanswer = document.getElementById("typeanswer").innerHTML;
 var TRAcaseinsensitive = therealanswer.toUpperCase();
 var theanswer = document.getElementById("answerbox").value;
 var TAcaseinsensitive = theanswer.toUpperCase();
-if (TAcaseinsensitive.includes(TRAcaseinsensitive)) {
-  document.getElementById("incorrect").display = "none";
-  document.getElementById("correct").display = "block";
-  }
- else {
-  document.getElementById("correct").style.display = "none";
-  document.getElementById("incorrect").style.display = "block";
-  }
+if (TAcaseinsensitive.includes(TRAcaseinsensitive)) 
+        document.getElementById("correct").style.display="block";
+  else
+        document.getElementById("incorrect").style.display="block";
 };
