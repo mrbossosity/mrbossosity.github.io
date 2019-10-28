9778b2729cac394f09009d20afafbd8545ee9cf9
mrbossosity <56628302+mrbossosity@users.noreply.github.com>
2019-10-28 11:09:16 -1000

    Update go-to-room.js

--- javascripts/go-to-room.js
+++ javascripts/go-to-room.js
@@ -1,5 +1,5 @@
 //Link enter to "go-to-room"
-document.getElementById("go-to-room")
+document.getElementById("room-name")
     .addEventListener("keydown", function(answerclick) {
     if (answerclick.keyCode === 13) {
         document.getElementById("go-to-room").click();
