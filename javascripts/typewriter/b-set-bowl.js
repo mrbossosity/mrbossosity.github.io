1d4996c6a836a9a585b482df98fb3f6146af5bc3
mrbossosity <56628302+mrbossosity@users.noreply.github.com>
2019-10-28 14:14:53 -1000

    Update b-set-bowl.js

--- javascripts/typewriter/b-set-bowl.js
+++ javascripts/typewriter/b-set-bowl.js
@@ -1,4 +1,4 @@
-var randomnumber = Math.floor((Math.random() * 9) + 0);
+var randomnumber = Math.floor((Math.random() * 8) + 0);
 var randomquestion = questions[randomnumber];
 var correspondinganswer = answers[randomnumber];
         //typewriter stuff
@@ -16,7 +16,7 @@ function type(){
 }
 function reset() {
   clearInterval(timer);
-  randomnumber = Math.floor((Math.random() * 9) + 0);
+  randomnumber = Math.floor((Math.random() * 8) + 0);
   randomquestion = questions[randomnumber];
   correspondinganswer = answers[randomnumber];
   sentence = randomquestion;
