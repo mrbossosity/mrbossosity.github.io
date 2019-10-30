8a8b5869e6250ff61222983994486a1b118b557f
mrbossosity <56628302+mrbossosity@users.noreply.github.com>
2019-10-30 07:21:17 -1000

    Update check.js

--- javascripts/check.js
+++ javascripts/check.js
@@ -1,8 +1,32 @@
+var total = 0;
+
+document.getElementById('add10').onclick = add10;
+document.getElementById('sub5').onclick = sub5;
+
+function zero() {
+  total = 0;
+  document.getElementById('pointstext').innerHTML = "Points: " + ""+total;
+}
+
+function add10() {
+  total = total + 10;
+  document.getElementById('pointstext').innerHTML = "Points: "+total;
+}
+
+function sub5() {
+  total = total - 5;
+  document.getElementById('pointstext').innerHTML = "Points: " + total;
+}
+
 function check() {
 var therealanswer = document.getElementById("typeanswer").innerHTML;
 var TRAcaseinsensitive = therealanswer.toUpperCase();
 var theanswer = document.getElementById("answerbox").value;
 var TAcaseinsensitive = theanswer.toUpperCase();
+if (TAcaseinsensitive.includes(TRAcaseinsensitive)) 
+        add10();
+  else
+        sub5();
 if (TAcaseinsensitive.includes(TRAcaseinsensitive)) 
         document.getElementById("correct").style.display="block";
   else
