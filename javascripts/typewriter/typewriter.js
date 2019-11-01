51298e6d8017e465a3e77265f454d935346589d4
mrbossosity <56628302+mrbossosity@users.noreply.github.com>
2019-11-01 06:58:11 -1000

    Update typewriter.js

--- javascripts/typewriter/typewriter.js
+++ javascripts/typewriter/typewriter.js
@@ -1,4 +1,4 @@
-var randomnumber = Math.floor((Math.random() * 366) + 0);
+var randomnumber = Math.floor((Math.random() * 447) + 0);
 var randomquestion = questions[randomnumber];
 var correspondinganswer = answers[randomnumber];
         //typewriter stuff
@@ -16,7 +16,7 @@ function type(){
 }
 function reset() {
   clearInterval(timer);
-  randomnumber = Math.floor((Math.random() * 366) + 0);
+  randomnumber = Math.floor((Math.random() * 447) + 0);
   randomquestion = questions[randomnumber];
   correspondinganswer = answers[randomnumber];
   sentence = randomquestion;
