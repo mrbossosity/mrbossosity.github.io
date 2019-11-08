871f6ab9fb0065d48d7013a6af7ef63127ccba92
mrbossosity <56628302+mrbossosity@users.noreply.github.com>
2019-11-08 12:09:18 -1000

    Update countries.js

--- javascripts/typewriter/niche/countries.js
+++ javascripts/typewriter/niche/countries.js
@@ -1,4 +1,4 @@
-var randomnumber = Math.floor((Math.random() * 63) + 0);
+var randomnumber = Math.floor((Math.random() * 95) + 0);
 var randomquestion = questions[randomnumber];
 var correspondinganswer = answers[randomnumber];
         //typewriter stuff
@@ -16,7 +16,7 @@ function type(){
 }
 function reset() {
   clearInterval(timer);
-  randomnumber = Math.floor((Math.random() * 63) + 0);
+  randomnumber = Math.floor((Math.random() * 95) + 0);
   randomquestion = questions[randomnumber];
   correspondinganswer = answers[randomnumber];
   sentence = randomquestion;
