3df407fd0e8fae10401eacad5edcf194362f1340
mrbossosity <56628302+mrbossosity@users.noreply.github.com>
2019-11-12 10:31:33 -1000

    Update Asian.js

--- javascripts/typewriter/niche/Asian.js
+++ javascripts/typewriter/niche/Asian.js
@@ -1,1 +1,40 @@
-f
+var randomnumber = Math.floor(Math.random() * array.length);
+var randomquestion = array[randomnumber].question;
+var correspondinganswer = array[randomnumber].ANSWER;
+        //typewriter stuff
+var target = document.getElementById("question");
+var char;
+var timer;
+var sentence;
+function type(){
+  if(char < sentence.length) {
+    target.innerHTML += sentence.charAt(char++);
+  } 
+  else {
+    clearInterval(timer);
+  } 
+}
+function reset() {
+  clearInterval(timer);
+  randomnumber = Math.floor(Math.random() * array.length);
+  randomquestion = array[randomnumber].question;
+  correspondinganswer = array[randomnumber].ANSWER;
+  sentence = randomquestion;
+  char = 0;
+  target.innerHTML = '';
+}
+function typeWriter() {
+  reset();  
+  var speed = document.getElementById("speedvalue").value;
+  timer = setInterval(type, speed);
+}
+function pause() {
+  clearInterval(timer)
+}
+function resume() {
+  var resumespeed = document.getElementById("speedvalue").value;
+  timer = setInterval(type, resumespeed);
+}
+function typeanswer() {
+  document.getElementById("typeanswer").innerHTML = correspondinganswer;
+}
