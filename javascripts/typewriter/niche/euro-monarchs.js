cbd85c9a9242c201663e2efd30ca7c9cc6a92499
mrbossosity <56628302+mrbossosity@users.noreply.github.com>
2019-11-12 08:58:09 -1000

    Update euro-monarchs.js

--- javascripts/typewriter/niche/euro-monarchs.js
+++ javascripts/typewriter/niche/euro-monarchs.js
@@ -1,1 +1,40 @@
-randomquestion = array[randomnumber].question;
+var randomnumber = Math.floor((Math.random() * 20) + 0);
+var randomquestion = array[randomnumber].question;
+var correspondinganswer = array[randomnumber].ANSWER;
+        //typewriter stuff
+var target = document.getElementById("question");
+var char;
+var timer;
+var sentence;
+function type(){
+  if(char < sentence.length) {
+    target.innerHTML += sentence.charAt(char++);
+  } 
+  else {
+    clearInterval(timer);
+  } 
+}
+function reset() {
+  clearInterval(timer);
+  randomnumber = Math.floor((Math.random() * 20) + 0);
+  randomquestion = array[randomnumber].question;
+  correspondinganswer = array[randomnumber].ANSWER;
+  sentence = randomquestion;
+  char = 0;
+  target.innerHTML = '';
+}
+function typeWriter() {
+  reset();  
+  var speed = document.getElementById("speedvalue").value;
+  timer = setInterval(type, speed);
+}
+function pause() {
+  clearInterval(timer)
+}
+function resume() {
+  var resumespeed = document.getElementById("speedvalue").value;
+  timer = setInterval(type, resumespeed);
+}
+function typeanswer() {
+  document.getElementById("typeanswer").innerHTML = correspondinganswer;
+}
