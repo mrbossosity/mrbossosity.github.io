9da6ea93d5e7278c80feaa38550e4efa98a75239
mrbossosity <56628302+mrbossosity@users.noreply.github.com>
2019-11-13 21:40:43 -1000

    Create simple-one-min.js

---
+++ javascripts/timer/simple-one-min.js
@@ -0,0 +1,12 @@
+<script>
+var timer
+var x = 60
+var y = 1
+function countdown() {
+    x -= y;
+    document.getElementById("timer").innerHTML = "00:" + x;
+}
+function startTimer() {
+    timer = setInterval(countdown, 1000)
+}
+</script>
