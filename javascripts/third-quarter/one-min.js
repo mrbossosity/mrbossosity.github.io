3962e6a37aa26c232e9d42f6e908318ddc0a59c7
mrbossosity <56628302+mrbossosity@users.noreply.github.com>
2019-11-14 11:52:27 -1000

    Update one-min.js

--- javascripts/third-quarter/one-min.js
+++ javascripts/third-quarter/one-min.js
@@ -1,10 +1,28 @@
 var timer
+var target = document.getElementById("timer")
 var x = 60
 var y = 1
+var randomnumber
 function countdown() {
     x -= y;
-    document.getElementById("timer").innerHTML = "00:" + x;
+    if (x < 10) {
+      target.innerHTML = "00:0" + x;
+    }
+    else {
+      target.innerHTML = "00:" + x;
+    }
 }
+function reset() {
+  clearInterval(timer);
+  target.innerHTML = "1:00";
+  $(".nodisplay").hide();
+  $(".ANSWER").html("ANSWER:");
+  $(".box").val("");
+  startquestion();
+  x = 60
+}
+
 function startTimer() {
-    timer = setInterval(countdown, 1000)
+    reset();
+    timer = setInterval(countdown, 1000);
 }
