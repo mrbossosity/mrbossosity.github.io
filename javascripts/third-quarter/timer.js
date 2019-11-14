65236f4e26c8e37530b7519edef087e82a910e8a
mrbossosity <56628302+mrbossosity@users.noreply.github.com>
2019-11-14 11:12:32 -1000

    Update timer.js

--- javascripts/third-quarter/timer.js
+++ javascripts/third-quarter/timer.js
@@ -2,14 +2,23 @@ var timer
 var target = document.getElementById("timer")
 var x = 60
 var y = 1
+var randomnumber
 function countdown() {
     x -= y;
-    target.innerHTML = "00:" + x;
+    if (x < 10) {
+      target.innerHTML = "00:0" + x;
+    }
+    else {
+      target.innerHTML = "00:" + x;
+    }
 }
-
 function reset() {
   clearInterval(timer);
-  target.innerHTML = "1:00"
+  target.innerHTML = "1:00";
+  $(".nodisplay").hide();
+  $(".ANSWER").html("ANSWER:");
+  $(".box").val("");
+  startquestion();
   x = 60
 }
 
